0d37c2194757e0e3d20c999b597e3c94ee94f460
helmi77 <helmi77@outlook.com>
2017-10-17 16:09:26 +0200

    Simplify triangle generation

--- problem15/euler15.c
+++ problem15/euler15.c
@@ -36,22 +36,22 @@ void parse_args(int argc, char* argv[], long *grid_size)
     }
 }
 
-void generate_triangle(uintmax_t ***triangle, long height)
+void generate_triangle(uintmax_t **triangle, long height)
 {
     for (long i = 0; i < height; ++i)
     {
         long columns = (i == 0) ? 1 : i + 2;
-        (*triangle)[i] = malloc(sizeof(uintmax_t) * columns);
+        triangle[i] = malloc(sizeof(uintmax_t) * columns);
         for (long j = 0; j < columns; ++j)
         {
             if (j == 0 || j == columns - 1)
-                (*triangle)[i][j] = 1;
+                triangle[i][j] = 1;
             else
             {
                 if (i == 1)
-                    (*triangle)[i][j] = 2;
+                    triangle[i][j] = 2;
                 else
-                    (*triangle)[i][j] = (*triangle)[i - 1][j - 1] + (*triangle)[i - 1][j];
+                    triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
             }
         }
     }
@@ -68,14 +68,14 @@ int main(int argc, char* argv[])
     parse_args(argc, argv, &grid_size);
     long rows = grid_size * 2;
 
-    if (grid_size == 0)
+    if (grid_size <= 0)
     {
         fprintf(stderr, "Grid size must be a positive integer\n");
         exit(EXIT_FAILURE);
     }
 
     uintmax_t **triangle = malloc(sizeof(uintmax_t*) * rows);
-    generate_triangle(&triangle, rows);
+    generate_triangle(triangle, rows);
 
     uintmax_t sum = get_paths(triangle, rows - 1);
     printf("%"PRId64"\n", sum);
