e1dc122979bea332ce557aa690e85e6fc6fd8f21
helmi77 <helmi77@outlook.com>
2017-10-17 20:17:47 +0200

    Prepare solution for problem 12

---
+++ problem12/euler12.c
@@ -0,0 +1,26 @@
+/*
+ * This program takes an integer as input and calculates
+ * the first triangle number which has a divisor count
+ * that is larger than the specified input.
+ *
+ * The n-th triangle number is defined as the sum of
+ * all integers up to and including n.
+ * e.g. 4-th triangle number: 1 + 2 + 3 + 4 = 10
+ *
+ * The divisors of n are defined as the integers up to
+ * and including n, whichevenly divide n.
+ * e.g. divisors of 6: 1, 2, 3, 6
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void parse_args(int argc, char *argv[], int *divisors)
+{
+}
+
+int main(int argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
