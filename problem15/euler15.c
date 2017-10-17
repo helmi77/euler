fc1951687ccf693e98a034317f97cc863b586647
helmi77 <helmi77@outlook.com>
2017-10-17 12:08:36 +0200

    Properly parse integer argument

--- problem15/euler15.c
+++ problem15/euler15.c
@@ -1,10 +1,12 @@
+#include <errno.h>
 #include <inttypes.h>
+#include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-void parse_args(int argc, char* argv[], int *grid_size)
+void parse_args(int argc, char* argv[], long *grid_size)
 {
     int opt = 0;
     while ((opt = getopt(argc, argv, "s:")) != -1)
@@ -12,7 +14,20 @@ void parse_args(int argc, char* argv[], int *grid_size)
         switch (opt)
         {
             case 's':
-                *grid_size = atoi(optarg);
+                errno = 0;
+                char *endptr;
+                *grid_size = strtol(optarg, &endptr, 10);
+                if ((errno == ERANGE && (*grid_size == LONG_MAX || *grid_size == LONG_MIN))
+                        || (errno != 0 && *grid_size == 0))
+                {
+                    perror("strtol");
+                    exit(EXIT_FAILURE);
+                }
+                if (endptr == optarg)
+                {
+                    fprintf(stderr, "Grid size must be an integer\n");
+                    exit(EXIT_FAILURE);
+                }
                 break;
             default:
                 fprintf(stderr, "Usage: %s -s gridsize\n", argv[0]);
@@ -21,13 +36,13 @@ void parse_args(int argc, char* argv[], int *grid_size)
     }
 }
 
-void generate_triangle(uintmax_t ***triangle, int height)
+void generate_triangle(uintmax_t ***triangle, long height)
 {
-    for (int i = 0; i < height; ++i)
+    for (long i = 0; i < height; ++i)
     {
-        int columns = (i == 0) ? 1 : i + 2;
+        long columns = (i == 0) ? 1 : i + 2;
         (*triangle)[i] = malloc(sizeof(uintmax_t) * columns);
-        for (int j = 0; j < columns; ++j)
+        for (long j = 0; j < columns; ++j)
         {
             if (j == 0 || j == columns - 1)
                 (*triangle)[i][j] = 1;
@@ -42,18 +57,18 @@ void generate_triangle(uintmax_t ***triangle, int height)
     }
 }
 
-uintmax_t get_paths(uintmax_t **triangle, int row)
+uintmax_t get_paths(uintmax_t **triangle, long row)
 {
     return triangle[row][(row + 2) / 2];
 }
 
 int main(int argc, char* argv[])
 {
-    int grid_size = -1;
+    long grid_size = -1;
     parse_args(argc, argv, &grid_size);
-    int rows = grid_size * 2;
+    long rows = grid_size * 2;
 
-    if (grid_size <= 0)
+    if (grid_size == 0)
     {
         fprintf(stderr, "Grid size must be a positive integer\n");
         exit(EXIT_FAILURE);
@@ -65,7 +80,7 @@ int main(int argc, char* argv[])
     uintmax_t sum = get_paths(triangle, rows - 1);
     printf("%"PRId64"\n", sum);
 
-    for (int i = 0; i < rows; ++i)
+    for (long i = 0; i < rows; ++i)
     {
         free(triangle[i]);
     }
