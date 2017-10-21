99f076a89ab80fb95024797dd686ba852a27d403
helmi77 <helmi77@outlook.com>
2017-10-21 16:09:05 +0200

    Add basic solution for problem 12

--- problem12/euler12.c
+++ problem12/euler12.c
@@ -12,15 +12,66 @@
  * e.g. divisors of 6: 1, 2, 3, 6
 */
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 void parse_args(int argc, char *argv[], int *divisors)
 {
+    int opt;
+    while ((opt = getopt(argc, argv, "d:")) != -1)
+    {
+        switch (opt)
+        {
+            case 'd':
+                *divisors = atoi(optarg);
+                break;
+            default:
+                fprintf(stderr, "Usage: %s -d divisors\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+}
+
+int get_divisors(int number)
+{
+    if (number == 1) return 1;
+
+    int i = 2, divisors = 0;
+    while (i <= sqrt(number))
+    {
+        if (number % i == 0)
+        {
+            ++divisors;
+            if (i != (number / i))
+            {
+                ++divisors;
+            }
+        }
+        ++i;
+    }
+    return divisors + 2;
 }
 
 int main(int argc, char *argv[])
 {
+    int min_divisors = 0;
+    parse_args(argc, argv, &min_divisors);
+    if (min_divisors <= 0)
+    {
+        fprintf(stderr, "Divisors must be a positive integer\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int divisor_count = 0, triangle_number = 0;
+    for (int i = 1; divisor_count <= min_divisors; ++i)
+    {
+        triangle_number += i;
+        divisor_count = get_divisors(triangle_number);
+    }
+
+    printf("%d has %d divisors\n", triangle_number, divisor_count);
+
     return EXIT_SUCCESS;
 }
